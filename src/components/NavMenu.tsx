af0f21cdfe1b064fbd515320efa5fe9a625ad473
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-09-19 20:38:27 -0400

    menu links

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,7 +1,15 @@
 import type { NavMenuStatus } from '@/types';
 
 const NavMenu = ({ active }: NavMenuStatus) => {
-  return <div data-active={active} className='navMenu'></div>;
+  return (
+    <div data-active={active} className='navMenu'>
+      <div className='navMenuLinks'>
+        <a href='http://medium.com'>Link 1</a>
+        <a href='http://medium.com'>Link 2</a>
+        <a href='http://medium.com'>Link 3</a>
+      </div>
+    </div>
+  );
 };
 
 export default NavMenu;
