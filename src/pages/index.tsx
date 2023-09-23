48fb45362d89c28b5b344a169fb6585929f73609
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-09-23 03:04:35 -0400

    clean index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head';
 import Layout from '@/components/Layout';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
