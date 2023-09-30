49d1dd2192a5d858d7d29f0c4f21552958089864
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-09-29 22:21:21 -0400

    no storybook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,15 @@
-import Head from 'next/head';
-import Layout from '@/components/Layout/Layout';
+import Layout from '@/components/Layout';
 
-const Home = () => {
+const Page = () => {
   return (
     <>
-      <Head>
-        <title>Animated Menu</title>
-        <meta
-          name='description'
-          content='how to create an immersive navigation menu with css transitions'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main>
-        <div style={{ height: '20rem' }}></div>
-        <h1 style={{ color: '#fff', textAlign: 'center' }}>
-          Attaching layout to page
-        </h1>
-        <div style={{ height: '100vh' }}></div>
-      </main>
+      <div>PAGE CONTENT</div>
     </>
   );
 };
 
-Home.getLayout = function getLayout(page: React.ReactElement) {
+Page.getLayout = function getLayout(page: React.ReactElement) {
   return <Layout>{page}</Layout>;
 };
 
-export default Home;
+export default Page;
